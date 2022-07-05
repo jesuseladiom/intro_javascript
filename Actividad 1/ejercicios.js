de176ec53db17bbef5b69240d6af875cc656da19
jesuseladiom <jesuseladiom@gmail.com>
2022-07-05 16:14:20 -0600

    Ejercicios de la Actividad 1

---
+++ Actividad 1/ejercicios.js
@@ -0,0 +1,21 @@
+// problema #1
+var numero= prompt("ingresa un numero")
+var res= numero * 2
+console.log("el resultado es: ", res)
+
+// problema #2
+var numero1= prompt("Escribe el Numero1:")
+var numero2= prompt("Escribe el Numero2:")
+console.log("la resta es: ", numero1-numero2)
+
+// problema #3
+const PI=3.141592
+var radio=5.3
+var area= PI * (radio**2)
+alert("el area es:" + area)
+
+//problema #4
+var diagonalMayor=8
+var diagonalMenor=6
+var area= diagonalMayor*diagonalMenor/2
+console.log("el area es: ", area)

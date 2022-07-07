e2c95ab64d2435d3315e6b0713cb5f05cd16259f
jesuseladiom <jesuseladiom@gmail.com>
2022-07-06 19:13:56 -0600

    Actividad 3

--- Vacunacion/logica.js
+++ Vacunacion/logica.js
@@ -4,12 +4,18 @@ var anioNacimiento= Number(prompt("Año de Nacimiento"));
 var municipio= prompt("Vives en un municipio de la frontera del Norte del pais (S/N)?");
 var semanas= Number(prompt("Ingresa la cantidad de semanas de Gestacion si estas embarazada (0=Sin Embarazo)"));
 
+document.write("Edad: " + edad + "<br>");
+document.write("Municipio: " + municipio+ "<br>");
+document.write("Semanas de Gestacion: " + semanas+ "<br>");
+document.write("Año de Nacimiento: " + anioNacimiento+ "<br>");
+
+
 //checa a que grupo pertenece
 if (edad >= 18 && (municipio=='s' || municipio=='S') )
     document.write("Soy Mayor de edad y vivo en la frontera norte del pais");
-else if (edad>=18 && semanas>9)
+else if (edad>=18 && semanas>=9)
     document.write("Embarazadas mayores de 18 años con mas de 9 semanas de gestacion");
-else if (2022 - anioNacimiento >= 30)
+else if ((2022 - anioNacimiento >= 30) && anioNacimiento>1800)
     document.write("Personas que cumplen 30 años o mas Este año");
 else
     document.write("No Pertenece a ningun grupo");
